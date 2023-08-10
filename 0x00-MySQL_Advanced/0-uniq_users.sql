e1f8974a5032377b62e6263cf24883d90ad2307d
MissJuly <joygitau98@gmail.com>
2023-08-10 22:48:06 +0300

    Fix errors

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,8 @@
 --script that creates a table users following these requirements
+-- With these attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
