f9bc298bae47425269beb5f48fb91c934aa392d0
MissJuly <joygitau98@gmail.com>
2023-08-11 01:26:35 +0300

    Add 100-average_weighted_score file

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,5 @@
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students.
+-- Requirements:
+-- Procedure ComputeAverageWeightedScoreForUsers is not taking any input.
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers
