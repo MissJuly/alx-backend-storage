d22029cd9e13c58ca1fc2d7d85324979c47831bc
MissJuly <joygitau98@gmail.com>
2023-08-15 16:45:53 +0300

    Add 11-schools_by_topic file

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module with function that returns the list
+of school having a specific topic
+"""
+
+
+def school_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school
+    """
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
