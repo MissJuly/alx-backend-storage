4f3b8ad09c8af1488537261494ec94530ced200b
MissJuly <joygitau98@gmail.com>
2023-08-15 16:25:42 +0300

    Add 8-all file

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    function that lists all documents in a collection
+    returns:
+      empty list if no document is in the collection
+    """
+    return [doc for doc in mongo_collection.find()]
